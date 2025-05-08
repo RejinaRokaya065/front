bd776cb4bc78b2ef34ee129f051dc91d9dfe8c65
RejinaRokaya065 <rokayarejina1@gmail.com>
2025-05-08 07:51:55 +0545

    js

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ function type(){
     }
 }
 type();
+
+//Projects
+const projects=[
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsible navigation bar  smooth animations.",
+    },
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsible navigation bar  smooth animations.",
+    },
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsible navigation bar  smooth animations.",
+    },
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsive navigation bar with",
+    },
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsible navigation bar  smooth animations.",
+    },
+    {
+        image:"image/Car.jpg",
+        title:"Animated Navbar",
+        description:"A responsive navigation bar with",
+    },
+]
+
+const container=document.getElementById("project-container");
+
+projects.forEach(project=>{
+    const card=document.createElement("div");
+    card.className="card";
+    card.innerHTML=`
+    <img src="${project.image}" alt="${project.title}"/>
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+    `;
+    container.appendChild(card);
+});
